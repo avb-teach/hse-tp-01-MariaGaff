b1e81eaf2364c113f4d5bad10ea8d7f7bc678a73
Maria <gaffarimaria@gmail.com>
2025-04-26 23:14:36 +0300

    Добавила реализацию для 3 теста в скрипте collect_files.s

--- collect_files.sh
+++ collect_files.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-if [[ "$#" -lt 2 || "$#" -gt 2 ]]; then
-    echo "Ошибка: необходимо указать две директории (входную и выходную)"
+if [[ "$#" -lt 2 || "$#" -gt 2 ]] 
+then
+    echo "Ошибка: необходимы две директории (входная и выходная)"
     exit 1
 fi
 
 input_dir="$1"
 output_dir="$2"
 
-if [ ! -d "$input_dir" ]; then
+if [ ! -d "$input_dir" ]
+then
     echo "Ошибка: не существует входной директории"
     exit 1
 fi
 
-if [ ! -d "$output_dir" ]; then 
+if [ ! -d "$output_dir" ]
+then 
     mkdir -p "$output_dir"
 fi
 
-find "$input_dir" -maxdepth 2 -type f -exec cp {} "$output_dir" \;
+find "$input_dir" -type f | while read -r file 
+do
+    base=$(basename "$file")
+    new_file="$output_dir/$base"
+    counter=1
+
+    while [[ -e "$new_file" ]] 
+    do
+        new_file="${output_dir}/${base%.*}($counter).${base##*.}"
+        ((counter++))
+    done
+
+    cp "$file" "$new_file"
+done
