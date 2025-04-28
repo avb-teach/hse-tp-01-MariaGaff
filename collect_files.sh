efac5be9e3f98faf5d88f52560fe8d24ff27ef2c
Maria <gaffarimaria@gmail.com>
2025-04-28 17:48:27 +0300

    Исправленный код

--- collect_files.sh
+++ collect_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [[ "$#" -lt 2 ]]; then
+if [[ "$#" -lt 2 ]]
+then
     echo "Ошибка: требуется указать входную и выходную директории."
     exit 1
 fi
@@ -9,37 +10,46 @@ input_directory="$1"
 output_directory="$2"
 max_depth=0
 
-if [[ "$#" -eq 4 && "$3" == "--max_depth" ]]; then
+if [[ "$#" -ge 4 && "$3" == "--max_depth" ]]
+then
     max_depth="$4"
 fi
 
-if [ ! -d "$input_directory" ]; then
+if [ ! -d "$input_directory" ]
+then
     echo "Ошибка: входная директория не найдена."
     exit 1
 fi
 
-if [ ! -d "$output_directory" ]; then
+if [ ! -d "$output_directory" ]
+then
     mkdir -p "$output_directory"
 fi
 
-find_command="find "$input_directory" -mindepth 1 -type f"
-if [ "$max_depth" -gt 0 ]; then
+find_command="find \"$input_directory\" -mindepth 1 -type f"
+if [ "$max_depth" -gt 0 ]
+then
     find_command+=" -maxdepth $max_depth"
 fi
 
-eval "$find_command" | while IFS= read -r current_file; do
-    relative_path="${current_file#$input_directory/}"
+eval "$find_command" | while IFS= read -r current_file
+do
+    relative_path="${current_file#"$input_directory"/}"
     destination_file="$output_directory/$relative_path"
     
     mkdir -p "$(dirname "$destination_file")"
 
     index=1
-    while [[ -e "$destination_file" ]]; do
-        destination_file="${output_directory}/${relative_path%.*}($index).${relative_path##*.}"
+    base_name="${destination_file%.*}"
+    extension="${destination_file##*.}"
+
+    while [[ -e "$destination_file" ]]
+    do
+        destination_file="${base_name}($index).${extension}"
         ((index++))
     done
 
     cp "$current_file" "$destination_file"
 done
 
-echo "Копирование завершено. Все файлы успешно скопированы в $output_directory."
+echo "Все файлы скопированы в $output_directory."
